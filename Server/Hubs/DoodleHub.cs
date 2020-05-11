0bd241dca7c595a476b0784c2bd02e16aa4895ce
Jim Paine <jim@jimpaine.io>
2020-05-11 20:58:42 +0100

    added clear

--- Server/Hubs/DoodleHub.cs
+++ Server/Hubs/DoodleHub.cs
@@ -1,17 +1,30 @@
 using System;
-using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authorization;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 using doodler.Client;
 
 namespace doodler.Server.Hubs
-{
+{    
     public class DoodleHub : Hub
     {
+        private readonly ILogger<DoodleHub> logger;
+
+        public DoodleHub(ILogger<DoodleHub> logger)
+        {
+            this.logger = logger;
+        }
+
         public async Task SendPayload(Payload payload)
         {
             await Clients.All.SendAsync("ReceivePayload", payload);
+        }
+
+        public async Task ClearCanvas()
+        {
+            await Clients.All.SendAsync("ClearCanvas");
         }
     }
 }
